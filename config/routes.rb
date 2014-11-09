82f697d19614c113b645c07a2c4b251b180245ac
Go-Faram <ruanjianzw@gmail.com>
2014-11-09 16:15:27 +0800

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	
+    sadfsadf	
 	root :to => "pages#welcome"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
